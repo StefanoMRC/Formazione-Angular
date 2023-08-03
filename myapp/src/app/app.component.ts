2de5d295d5a21836e4835f1c602681fa057dbed2
StefanoMRC <stefano.mrc92@gmail.com>
2023-08-03 11:52:08 +0200

    pipe

--- myapp/src/app/app.component.ts
+++ myapp/src/app/app.component.ts
@@ -7,9 +7,21 @@ import { Component } from "@angular/core";
     <app-hello *ngIf="visible"></app-hello>
 
     <li [hidden]="!visible" *ngFor="let user of users">{{ user }}</li>
+    <!-- DATE PIPE -->
+    <div>{{ today | date : "dd/MMM/yy" }}</div>
+    <!-- CURRENCY PIPE -->
+    <div>{{ money | currency : "€" }}</div>
+    <!-- NUMBER PIPE -->
+    <div>{{ bitcoins | number : "2.2-4" }}</div>
+    <!-- NUMBER OBJ -->
+    <pre>{{ yourJson | json }}</pre>
   `,
 })
 export class AppComponent {
   visible = false;
   users = ["Stefano", "Fabio", "Andrea"];
+  today = Date.now();
+  money = 1200;
+  bitcoins = 0.12345242;
+  yourJson = { id: 1, name: "Stefano" };
 }
